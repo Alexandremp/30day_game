6bcba9f0436abf938c232925e722ec8af35794b4
Alexandre Pereira <33624800+Alexandremp@users.noreply.github.com>
2018-04-13 15:55:30 +0100

    Update UserForm.php

--- prefabs/login/UserForm.php
+++ prefabs/login/UserForm.php
@@ -2,5 +2,55 @@
 
 namespace AppBundle\Entity;
 class StundentForm{
-    private 
-    private $UserId;
+    private $userName;
+    private $userId;
+    public  $password;
+    private $email;
+    private $birthday;
+    
+    public function getUserName() {
+        return $this->userName;
+        
+    }
+    
+    public function setUserName(){
+        $this->userName =$userName;
+    }
+
+    public function getUserId() {
+        return $this->userId;
+        
+    }
+    
+    public function setUserId(){
+        $this->userId =$userId;
+    }
+    
+    public function getPassword() {
+        return $this->password;
+        
+    }
+    
+    public function setPassword(){
+        $this->password=$password;
+    }
+
+    public function getEmail() {
+        return $this->email;
+        
+    }
+    
+    public function setEmail(){
+        $this->email=$email;
+    }
+    
+    public function getBirthday() {
+        return $this ->birthday;
+        
+    }
+    
+    public function setBirthday(){
+        $this->userId =$birthday;
+    }
+    
+?>
