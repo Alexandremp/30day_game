337901664a2c3117afbb98683c40a73325027443
Alexandre Pereira <33624800+Alexandremp@users.noreply.github.com>
2018-05-18 15:43:45 +0100

    Create UserDetails.php

---
+++ prefabs2/template/Entity/UserDetails.php
@@ -0,0 +1,46 @@
+<?php
+ 
+ namespace App\Entity;
+ 
+ use Doctrine\ORM\Mapping as ORM;
+ 
+ /*
+ * @ORM\Entiy(repository="App\Repository\UserCryptRepository")
+ */
+ 
+ class Article
+ {
+ 
+ /**
+  * @ORM\Id()
+  * @ORM\GeneralValue()
+  * @ORM\Colum(type="integer")
+  */
+  
+  private $id;
+  
+  /**
+  * @ORM\Column(type="text")
+  */
+  
+  private username;
+  
+  /**
+  * @ORM\Column(type="text")
+  */
+  
+  private $email;
+  
+  /**
+  * @ORM\Column(type="text")
+  */
+  
+  private $password;
+  
+  /**
+  *@Id @OneToOne(targetEntity="User")
+  */
+  
+  private $user_account_id;
+}
+?>
