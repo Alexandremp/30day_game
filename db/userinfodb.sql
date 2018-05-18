a5955299ad12990157ecbc4455d48469ca42fc39
Alexandre Pereira <33624800+Alexandremp@users.noreply.github.com>
2018-05-18 15:13:41 +0100

    Update userinfodb.sql

--- db/userinfodb.sql
+++ db/userinfodb.sql
@@ -1,14 +1,15 @@
 
 CREATE TABLE user_crypt(
    id                                        SERIAL         PRIMARY KEY,
-   user_account_id                           INT            REFERENCES GALAXY(ID),
+   user_account_id                           INT            REFERENCES user_acccount(ID),
    password_salt                             TEXT(100)      NOT NULL,
    password_pepper                           TEXT(100)      NOT NULL,
    password_hash                             TEXT(100)      NOT NULL
 );
 
 CREATE TABLE user_details(
-   id                                        SERIAL    PRIMARY KEY,
+   id                                        SERIAL         PRIMARY KEY,
+   user_account_id                           INT            REFERENCES user_acccount(ID),
    username                                  TEXT(255)      NOT NULL,
    email                                     TEXT(100)      NOT NULL,
    password                                  TEXT(255)      NOT NULL
@@ -18,13 +19,12 @@ CREATE TABLE user_details(
 CREATE TABLE user_account(
    id                                        SERIAL         PRIMARY KEY,
    screen_user_name                          TEXT(255)      NOT NULL,
-   user_account_id                           INT            REFERENCES GALAXY(ID),
 );
 ////////////Controller Connection//////////
 CREATE TABLE user_external_login(
    id                                        SERIAL    PRIMARY KEY,
-   user_account_id                           INT       REFERENCES GALAXY(ID),
-   external_autentication_provider_id        INT       REFERENCES GALAXY(ID),
+   user_account_id                           INT       REFERENCES user_account(ID),
+   external_autentication_provider_id        INT       REFERENCES external_autentication_provider(ID),
    name                                      TEXT(30)  NOT NULL,
    first_name                                TEXT(30)  NOT NULL,
    last_name                                 TEXT(30)  NOT NULL,
@@ -34,9 +34,9 @@ CREATE TABLE user_external_login(
 
 CREATE TABLE async_operation(
    id                                        SERIAL    PRIMARY KEY,
-   external_autentication_provider_id        INT       REFERENCES GALAXY(ID),
-   async_operation_status_type_id            INT       REFERENCES GALAXY(ID),
-   user_external_login_id                    INT       REFERENCES GALAXY(ID)
+   external_autentication_provider_id        INT       REFERENCES external_autentication_provider(ID),
+   async_operation_status_type_id            INT       REFERENCES async_operation_status_type(ID),
+   user_external_login_id                    INT       REFERENCES user_external_login(ID)
 );
 
 
