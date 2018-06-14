39beafde10d7b00464798c9784fd14870b1bd1b6
Alexandre Pereira <33624800+Alexandremp@users.noreply.github.com>
2018-06-14 15:34:23 +0100

    Update BuildTaskController.php

--- try3/src/Controller/BuildTaskController.php
+++ try3/src/Controller/BuildTaskController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+// ...
+use App\Entity\Building;
+use App\Entity\BuildingTodo;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class BuildTaskController extends Controller
 {
@@ -16,4 +22,46 @@ class BuildTaskController extends Controller
             'controller_name' => 'BuildTaskController',
         ]);
     }
+    
+     /**
+     * @Route("/building_task_creation/{id}", name="builtask_new")
+     *
+     */
+    public function BuildStart($id)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $buildings = $entityManager->getRepository(Building::class)->find($id);
+        if (!$buildings) {
+            throw $this->createNotFoundException('No product found for id '.$id);
+        }
+        $level=$buildings->getControlHQ();
+        $level=$level+1;
+        $buildings->setControlHQ($level);
+        $entityManager->flush();
+        
+        
+        $entityManager = $this->getDoctrine()->getManager();
+        $buildtask = new BuildingTodo();
+        $buildtask->setNumberOfTasks(1);
+        $buildtask->setNumberOfFreeTasks(0);
+        $buildtask->setTaskRemaingTime(null);
+        $buildtask->setTaskRemaingTime2(null);
+        $buildtask->setTask1(0);
+        $buildtask->setTask2(0);
+        $buildtask->setTask3(0);
+        $buildtask->setTask4(0);
+        $buildtask->setTask5(0);
+        $buildtask->setTask6(0);
+        $buildtask->setTask7(0);
+        $buildtask->setTask8(0);
+        $buildtask->setTask9(0);
+        $buildtask->setTask10(0);
+        
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($buildtask);
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+        return new Response('Saved new Task with id ' . $buildings->getId());
+    }
+    
 }
